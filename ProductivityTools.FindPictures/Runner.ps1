d07e64baf45b547ee27ccdf77df3239dfcae8f00
Pawel Wujczyk <pwujczyk@hotmail.com>
2021-04-26 12:41:17 +0200

    rename

--- ProductivityTools.FindPictures/Runner.ps1
+++ ProductivityTools.FindPictures/Runner.ps1
@@ -1,6 +1,6 @@
 cd $PSScriptRoot
 clear
-Import-Module .\ProductivityTools.PSFindPictures.psm1 -Force
+Import-Module .\ProductivityTools.FindPictures.psm1 -Force
 
 cd d:\.PawelPC\Photographs\
 Find-Pictures -verbose -CopyPicturesToDirectory d:\Trash\xx 
